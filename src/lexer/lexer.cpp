4eb8ee513be217f1bb8573fde1537ba488dc6b13
Grant <gbaumstark@micds.org>
2026-08-20 03:07:38 -0500

    working lexer! the tokens work and come out i just have to get down to the meat and potateos of it now. That is a tommorow thing im thinking tho.

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -12,9 +12,47 @@ namespace split {
     
     //function for the actual lexing
     std::vector<Token> Lexer::lex() {
-        //lit just returns an empty vector for now
+        //create our empty vector of tokens
         std::vector<Token> v;
-        return v;
+        
+        //start going thru char by char to create tokens
+        while(!atEnd()){
+            //look at the current char
+            char c = peek();
+
+            if(c == ' ' || c == '\t'){
+                advance(); //this way whitespace IN THE CODE is ignored so i can make as many spaces and such as i want
+            }
+            else if(c == '\n'){
+                Token t;
+                t.type = Tok::NEWLINE;
+                t.line = line_;
+                line_++;
+
+                //adds this token to the array of tokens
+                v.push_back(t);
+                advance();
+                
+            }
+            else if(c == '#'){
+                while(peek() != '\n' && !atEnd()){
+                    advance();
+                }
+            }
+            //else pretty much covers everything right now. This will be a lot of logic later.
+            else {
+                advance();
+            }
+
+        }
+
+        //now that we are outside of the loop make the end token and add it to vector
+        Token t;
+        t.type = Tok::END;
+        t.line = line_;
+        v.push_back(t);
+        
+        return v; //all the tokens
     }
 }
 
